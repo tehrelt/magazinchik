198bd5d7b9cddb970e7012c1d1b907ef78dfd238
relt <haxrelt@gmail.com>
2023-12-01 16:41:13 +0300

    create a ManufacturerController with 2 /GET endpoints

--- magazinchik/Controllers/ManufacturerController.cs
+++ magazinchik/Controllers/ManufacturerController.cs
@@ -1,6 +1,36 @@
+using maganzinchik.DAL;
+using maganzinchik.DAL.domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 namespace magazinchik.Controllers;
 
-public class ManufacturerController
+[ApiController]
+[Route("api/v1/[controller]")]
+public class ManufacturerController : ControllerBase
 {
-    
+    private SneakersShopContext _context;
+    public ManufacturerController(SneakersShopContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Manufacturer>>> Get()
+    {
+        return await _context.Manufacturers.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Manufacturer>> GetById(ulong id)
+    {
+        Manufacturer manufacturer = _context.Manufacturers.FirstOrDefault(m => m.Id == id);
+
+        if (manufacturer == null)
+        {
+            return NotFound();
+        }
+
+        return manufacturer;
+    }
 }
